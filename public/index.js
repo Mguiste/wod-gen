30c9de9b3baba252bdd37c43162709db69d4d5e5
mguiste <mguiste@live.com>
2020-06-11 21:29:42 -0700

    Empty handler methods for log in an new profile

--- public/index.js
+++ public/index.js
@@ -10,5 +10,24 @@
   window.addEventListener('load', init)
 
   function init () {
+    id('log-in').addEventListener('click', logInClick)
+    id('new-profile').addEventListener('click', newProfileClick)
+  }
+
+  // -------------------- EVENT HANDLER FUNCTIONS -------------------- //
+  function logInClick () {
+  }
+
+  function newProfileClick () {
+  }
+
+  // -------------------- HELPER FUNCTIONS -------------------- //
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id (null if none).
+   */
+  function id (idName) {
+    return document.getElementById(idName)
   }
 })()
