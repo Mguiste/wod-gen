51c9378c30328a1b080869a481e19c7307fddc46
mguiste <mguiste@live.com>
2020-06-11 19:19:42 -0700

    Basic frontend js for front page

---
+++ public/index.js
@@ -0,0 +1,14 @@
+/**
+ * Name: Mason Guiste
+ * Date: 6/11/20
+ *
+ * The frontend JavaScript file for wod gens intro page.
+ */
+
+'use strict'
+;(function () {
+  window.addEventListener('load', init)
+
+  function init () {
+  }
+})()
