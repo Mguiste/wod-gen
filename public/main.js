6421b96f631209d82dbc29d7b4ed6ff45f279b20
mguiste <mguiste@live.com>
2020-06-15 10:16:17 -0700

    Main page requests equipment and displays them

--- public/main.js
+++ public/main.js
@@ -18,9 +18,18 @@
     }
   }
 
-  function init () {
+  async function init () {
     id('profile').textContent = window.sessionStorage.getItem('name')
     id('log-out').addEventListener('click', logOutClick)
+    const htmlEquipment = id('equipment')
+    const equipment = await getAllEquipment()
+    equipment.forEach(e => {
+      const div = gen('div')
+      const p = gen('p')
+      p.textContent = e
+      div.appendChild(p)
+      htmlEquipment.appendChild(div)
+    })
   }
 
   // -------------------- EVENT HANDLER FUNCTIONS -------------------- //
@@ -29,6 +38,38 @@
     window.sessionStorage.removeItem('name')
     window.sessionStorage.removeItem('equipment_ids')
     window.location.replace('/')
+  }
+
+  // -------------------- API REQUEST FUNCTIONS -------------------- //
+  async function getAllEquipment () {
+    const response = await window.fetch('allequipment')
+    checkStatus(response)
+    return response.json()
+  }
+
+  // -------------------- HELPER FUNCTIONS -------------------- //
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} response - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   *                    Promise result
+   */
+  function checkStatus (response) {
+    if (response.ok) {
+      return response
+    } else {
+      throw Error('Error in request: ' + response.statusText)
+    }
+  }
+
+  /**
+   * Generates a new document element with the given tagName.
+   * @param {string} tagName the name of the tag to create
+   * @returns {object} DOM created object with the given tag.
+   */
+  function gen (tagName) {
+    return document.createElement(tagName)
   }
 
   /**
