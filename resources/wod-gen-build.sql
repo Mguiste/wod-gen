ecd0711ea38c1b7e9e8d6c3c6d6de88043d3723e
mguiste <mguiste@live.com>
2020-06-15 20:33:41 -0700

    Creates equipment table

--- resources/wod-gen-build.sql
+++ resources/wod-gen-build.sql
@@ -3,3 +3,19 @@ CREATE TABLE profiles (
   name TEXT,
   equipment_ids TEXT
 );
+
+CREATE TABLE equipment (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+INSERT INTO equipment (name) VALUES
+  ("Barbells"),
+  ("Dumbbells"),
+  ("Row"),
+  ("Bike"),
+  ("Skierg"),
+  ("Wall Balls"),
+  ("Kettle Bell"),
+  ("Pull Up Bar"),
+  ("Sled");
