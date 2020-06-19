0998745844788cc9c265a1c9f0bb0a24d544ac92
mguiste <mguiste@live.com>
2020-06-18 17:40:40 -0700

    Test file for index page

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,52 @@
+/**
+ * Name: Mason Guiste
+ * Date: 6/17/20
+ *
+ * The test file for index page.
+ */
+
+function shouldShowMessage (action, success) {
+  action()
+  cy.get('.message')
+  cy.get('.message > div').should('have.class', success ? 'success' : 'error')
+  cy.get('.message > div > p')
+  cy.get('.message > div > button').click()
+  cy.get('.message').should('not.exist')
+}
+
+describe('Index page tests', () => {
+  beforeEach(() => {
+    cy.exec('npm run reset:database')
+    cy.visit('/')
+  })
+  describe('Login', () => {
+    it('login no profile', () => {
+      shouldShowMessage(() => cy.get('#login').click(), false)
+    })
+    it('login profile does not exist', () => {
+      cy.get('#profile').type('jackson')
+      shouldShowMessage(() => cy.get('#login').click(), false)
+    })
+    it('login profile does exist', () => {
+      cy.get('#profile').type('jackson')
+      shouldShowMessage(() => cy.get('#new-profile').click(), true)
+      cy.get('#login').click()
+      cy.location().should(loc => assert.strictEqual(loc.pathname, '/main.html'))
+    })
+  })
+  describe('CreateProfile', () => {
+    it('create profile', () => {
+      cy.get('#profile').type('jackson')
+      shouldShowMessage(() => cy.get('#new-profile').click(), true)
+    })
+    it('create existing profile', () => {
+      cy.get('#profile').type('jackson')
+      shouldShowMessage(() => cy.get('#new-profile').click(), true)
+      shouldShowMessage(() => cy.get('#new-profile').click(), false)
+    })
+    it('create profile no profile', () => {
+      shouldShowMessage(() => cy.get('#new-profile').click(), false)
+    })
+  })
+  after(() => cy.exec('npm run reset:database'))
+})
